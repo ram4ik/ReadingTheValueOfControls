61f55bd7ee3d4b85616a29decea9dd45399df066
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-06 09:39:53 +0200

    Reading the value of controls

--- ReadingTheValueOfControls/ContentView.swift
+++ ReadingTheValueOfControls/ContentView.swift
@@ -9,8 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var estimate = 25.0
+    @State private var rating = 3
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Slider(value: $estimate, in: 0...50)
+                .padding()
+                .accessibility(value: Text("\(Int(estimate))"))
+            
+            Stepper("Rate our services: \(rating)/5", value: $rating, in: 1...5)
+                .accessibility(value: Text("\(rating) out of 5"))
+        }
     }
 }
 
